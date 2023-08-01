ba979af29ecb7470fefa2b633f95e3554314d2dd
CharlesTin0122 <tianchao0533@gmail.com>
2023-08-01 10:05:13 +0800

    deleted:    .idea/.gitignore 	deleted:    .idea/inspectionProfiles/Project_Default.xml 	deleted:    .idea/inspectionProfiles/profiles_settings.xml 	deleted:    .idea/misc.xml 	deleted:    .idea/modules.xml 	deleted:    .idea/tcTools.iml 	deleted:    .idea/vcs.xml 	modified:   scripts/Animation/CurveFilter.py 	deleted:    scripts/Animation/__pycache__/CurveFilter.cpython-39.pyc 	deleted:    scripts/Animation/__pycache__/FbxExporter.cpython-39.pyc 	deleted:    scripts/Animation/__pycache__/FbxImporter.cpython-39.pyc 	deleted:    scripts/Animation/__pycache__/PoseTools.cpython-39.pyc 	deleted:    scripts/Rigging/__pycache__/BatchConstrainObj.cpython-39.pyc 	deleted:    scripts/ScriptPackages/animCurveFilter/CurveFilter.py 	deleted:    scripts/ScriptPackages/animCurveFilter/CurveFilterFunctions.py 	deleted:    scripts/ScriptPackages/animCurveFilter/Filter_UI2.ui 	deleted:    scripts/ScriptPackages/animCurveFilter/__init__.py 	deleted:    scripts/ScriptPackages/animCurveFilter/__pycache__/CurveFilter.cpython-39.pyc 	deleted:    scripts/ScriptPackages/animCurveFilter/__pycache__/CurveFilterFunctions.cpython-39.pyc 	deleted:    scripts/ScriptPackages/animCurveFilter/__pycache__/__init__.cpython-39.pyc 	new file:   scripts/ScriptPackages/animCurveFilter/anim_curve_filter.py 	deleted:    scripts/ScriptPackages/batchConstrNearestObj/__pycache__/batch_constrain_obj.cpython-39.pyc 	deleted:    scripts/ScriptPackages/fbxExporter/__pycache__/fbx_exporter.cpython-39.pyc 	deleted:    scripts/ScriptPackages/fbxImporter/__pycache__/fbx_importer.cpython-39.pyc 	deleted:    scripts/ScriptPackages/generateDeformer/__pycache__/generate_deformer.cpython-39.pyc 	deleted:    scripts/ScriptPackages/mel2Py/__pycache__/mel2py.cpython-39.pyc 	deleted:    scripts/ScriptPackages/modelChecker/__pycache__/__init__.cpython-39.pyc 	deleted:    scripts/ScriptPackages/modelChecker/__pycache__/modelChecker_UI.cpython-39.pyc 	deleted:    scripts/ScriptPackages/modelChecker/__pycache__/modelChecker_commands.cpython-39.pyc 	deleted:    scripts/ScriptPackages/modelChecker/__pycache__/modelChecker_list.cpython-39.pyc 	deleted:    scripts/ScriptPackages/packageTest.pyc 	deleted:    scripts/ScriptPackages/poseTools/__pycache__/pose_tools.cpython-39.pyc 	deleted:    scripts/ScriptPackages/reParent/__pycache__/reParent.cpython-39.pyc 	modified:   scripts/ScriptPackages/reParent/reParent.py 	deleted:    scripts/ScriptPackages/reParent/reParent.pyc 	deleted:    scripts/TD/__pycache__/GenerateDeformer.cpython-39.pyc 	deleted:    scripts/TD/__pycache__/Mel2Pymel.cpython-39.pyc 	deleted:    scripts/TD/__pycache__/ModelChecker.cpython-39.pyc

--- scripts/Animation/CurveFilter.py
+++ scripts/Animation/CurveFilter.py
@@ -6,6 +6,7 @@
 # @Software : PyCharm
 # Description:
 from importlib import reload
-import animCurveFilter.CurveFilter as Filter
+import animCurveFilter.anim_curve_filter as Filter
 reload(Filter)
-Filter.main()
+curve_filter = Filter.AnimCurveFilter()
+curve_filter.create_ui()
