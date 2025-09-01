c362ac55a6f8c8c3dfd90410cc6bef023b8e7eed
CharlesTin0122 <tianchao0533@gmail.com>
2025-09-01 11:42:15 +0800

    修改环形Ribbon的bug

--- scripts/ScriptPackages/ribbonCreator/matrix_ribbon.py
+++ scripts/ScriptPackages/ribbonCreator/matrix_ribbon.py
@@ -41,8 +41,8 @@ class RibbonCreator(QtWidgets.QDialog):
 
         self.inner_curve = None
         self.outer_curve = None
-        self.flip_normal = False
-        self.closed_loop = False
+        self.is_flip_normal = False
+        self.is_circular_plane = False
 
         self.ribbon_pin_num = 5
         self.ribbon_ctrl_num = 3
@@ -92,7 +92,7 @@ class RibbonCreator(QtWidgets.QDialog):
         self.outer_edges_btn = QtWidgets.QPushButton("Outer egdes")
         self.outer_edges_btn.setFixedWidth(120)
         self.flip_normal_cb = QtWidgets.QCheckBox("Flip Normals")
-        self.closed_loop_cb = QtWidgets.QCheckBox("Closed Loop")
+        self.closed_loop_cb = QtWidgets.QCheckBox("Circular Plane")
 
         self.p2n_btn = QtWidgets.QPushButton("Polygon To Nurbs")
 
@@ -242,47 +242,33 @@ class RibbonCreator(QtWidgets.QDialog):
 
     @QtCore.Slot()
     def on_inner_btn_clicked(self):
-        self.inner_curve = pm.polyToCurve(
-            form=2,  # 曲线的形式: 0: 开放曲线 (Open)1: 闭合曲线 (Closed)2: 周期性曲线 (Periodic)，适合生成无缝闭合的曲线。
-            degree=3,  # 曲线的度:1（线性）或 3（三次样条曲线，平滑）
-            conformToSmoothMeshPreview=0,  # 是否使用均匀间距: 0: 非均匀间距 1:强制均匀间距。
-            name=f"{self.ribbon_name}_iner_cuv",
-        )[0]
+        self.inner_curve = self.polygon_to_curve(
+            curve_name=f"{self.ribbon_name}_iner_cuv",
+        )
+        return self.inner_curve
 
     @QtCore.Slot()
     def on_outer_btn_clicked(self):
-        self.outer_curve = pm.polyToCurve(
-            form=2,
-            degree=3,
-            conformToSmoothMeshPreview=0,
-            name=f"{self.ribbon_name}_outer_cuv",
-        )[0]
-        # pm.rebuildCurve("temp2", rpo=1, rt=0, s=20, ch=1)
+        self.outer_curve = self.polygon_to_curve(
+            curve_name=f"{self.ribbon_name}_outer_cuv",
+        )
+        return self.outer_curve
 
     @QtCore.Slot()
     def on_flip_normal_cb_toggled(self, check):
-        self.flip_normal = check
+        self.is_flip_normal = check
 
     @QtCore.Slot()
     def on_closed_loop_cb_toggled(self, check):
-        self.closed_loop = check
+        self.is_circular_plane = check
 
     @QtCore.Slot()
     def on_p2n_btn_clicked(self):
-        self.nurbs_plane = pm.loft(
+        self.nurbs_plane = self.polygon_to_nurbs(
+            self.ribbon_name,
             self.inner_curve,
             self.outer_curve,
-            reverse=0,  # 是否反转曲线的方向
-            constructionHistory=0,  # 是否保留构造历史
-            uniform=1,  # 是否使用均匀参数化。
-            close=self.closed_loop,  # 是否闭合曲线
-            autoReverse=1,  # 是否自动调整曲线方向以确保一致性
-            degree=1,  # 生成曲面的 U 方向度数
-            sectionSpans=1,  # 曲面在 V 方向的跨度数
-            range=0,  # 是否使用完整曲线范围
-            polygon=0,  # 是否生成多边形曲面。
-            reverseSurfaceNormals=self.flip_normal,  # 是否反转曲面法线
-            name=f"{self.ribbon_name}_plane",
+            self.is_flip_normal,  # 是否反转曲面法线
         )
         pm.delete(self.inner_curve)
         pm.delete(self.outer_curve)
@@ -358,8 +344,39 @@ class RibbonCreator(QtWidgets.QDialog):
             u=1,
             v=segment_count,
             constructionHistory=0,
-        )[0]
-        return nurbs_plane
+        )
+        pm.select(clear=True)
+        return nurbs_plane[0]
+
+    def polygon_to_curve(self, curve_name):
+        nirbs_curve = pm.polyToCurve(
+            form=2,  # 曲线的形式: 0: 开放曲线 (Open)1: 闭合曲线 (Closed)2: 周期性曲线 (Periodic)，适合生成无缝闭合的曲线。
+            degree=3,  # 曲线的度:1（线性）或 3（三次样条曲线，平滑）
+            conformToSmoothMeshPreview=0,  # 是否使用均匀间距: 0: 非均匀间距 1:强制均匀间距。
+            name="curve_name",
+        )
+        pm.select(clear=True)
+        # pm.rebuildCurve("temp2", rpo=1, rt=0, s=20, ch=1)
+        return nirbs_curve[0]
+
+    def polygon_to_nurbs(self, ribbon_name, inner_curve, outer_curve, is_flip_nornal):
+        nurbs_plane = pm.loft(
+            inner_curve,
+            outer_curve,
+            reverse=0,  # 是否反转曲线的方向
+            constructionHistory=0,  # 是否保留构造历史
+            uniform=1,  # 是否使用均匀参数化。
+            close=0,  # 是否闭合曲面，如果设置为true,则生成的表面将是封闭的(周期性)的,起点(终点)都在第一条曲线处。
+            autoReverse=1,  # 自动调整曲面方向
+            degree=3,  # 生成曲面的度数
+            sectionSpans=1,  # 曲面的跨度数
+            range=0,  # 是否使用完整曲线范围
+            polygon=0,  # 是否生成多边形曲面。
+            reverseSurfaceNormals=is_flip_nornal,  # 是否反转曲面法线，通过UV交换来实现
+            name=f"{ribbon_name}_plane",
+        )
+        pm.select(clear=True)
+        return nurbs_plane[0]
 
     def create_ribbon(
         self, nurbs_plane, ribbon_name, pin_num, ctrl_num, create_ctrl=False
@@ -398,11 +415,13 @@ class RibbonCreator(QtWidgets.QDialog):
         nurbs_shape = nurbs_plane.getShape()
         # 创建pin骨骼
         pin_jnt_list = []
-        # 如果是闭合曲面（首尾相接），少创建一个pin骨骼
-        index_pin_num = pin_num - 1 if self.closed_loop else pin_num
-        for i in range(index_pin_num):
+
+        for i in range(pin_num):
             # 根据pin_num计算位置比例，并且防止除0
             v_pose = 0.0 if pin_num == 1 else (i / float(pin_num - 1))
+            # 如果是闭合曲面（首尾相接），则多计算一个段数，防止首尾骨骼重合
+            if self.is_circular_plane:
+                v_pose = i / float(pin_num)
             uvPin_node = pm.createNode("uvPin", name=f"{ribbon_name}_uvPin_{i}")
             pin_jnt = pm.joint(name=f"{ribbon_name}_pin_{i}", radius=1)
 
@@ -416,9 +435,11 @@ class RibbonCreator(QtWidgets.QDialog):
         ctrl_jnt_list = []
         ctrl_grp_list = []
         # 如果是闭合曲面（首尾相接），少创建一个控制骨骼
-        index_ctrl_num = ctrl_num - 1 if self.closed_loop else ctrl_num
-        for i in range(index_ctrl_num):
+        for i in range(ctrl_num):
             v_pose = 0 if ctrl_num == 1 else (i / float(ctrl_num - 1))
+            # 如果是闭合曲面（首尾相接），则多计算一个段数，防止首尾骨骼重合
+            if self.is_circular_plane:
+                v_pose = i / float(ctrl_num)
             posi_node = pm.createNode(
                 "pointOnSurfaceInfo", name=f"{ribbon_name}_posi_{i}"
             )
