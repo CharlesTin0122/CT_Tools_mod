2889f3df847e0d8fe3934f8d9e74198781380be3
CharlesTin0122 <tianchao0533@gmail.com>
2025-09-29 18:22:34 +0800

    添加扭曲计算算法

--- scripts/ScriptPackages/RigUtils/rig_utils.py
+++ scripts/ScriptPackages/RigUtils/rig_utils.py
@@ -710,7 +710,7 @@ def matrix_constraint(
     translate=True,
     rotate=True,
     scale=True,
-    target_is_joint=True,
+    target_is_joint=False,
 ):
     """
     矩阵约束.
@@ -802,3 +802,117 @@ def create_export_joints(source_jnts=None, sufix="exp"):
         matrix_constraint(jnt, exp_jnt, maintainOffset=False, target_is_joint=True)
         exp_jnt_list.append(exp_jnt)
     return exp_jnt_list
+
+
+def connect_twist_swing(
+    driver=None,
+    driven=None,
+    twist: float = 0.0,
+    swing: float = 0.0,
+    twist_axis: str = "Y",
+    driven_is_joint=False,
+):
+    # 获取对象
+    if not driver or not driven:
+        selection = pm.selected()
+        if len(selection) < 2:
+            pm.warning("请选择至少两个对象：第一个是源对象，第二个是目标对象。")
+            return None
+        driver, driven = selection[0], selection[1]
+    with pm.UndoChunk():
+        # 添加属性
+        for attr in ["twist", "swing"]:
+            if not driven.hasAttr(attr):
+                driven.addAttr(
+                    attr,
+                    type=float,
+                    minValue=0,
+                    maxValue=1,
+                    defaultValue=0,
+                    keyable=True,
+                )
+        driven.twist.set(twist)
+        driven.swing.set(swing)
+        # 核心逻辑
+        ## 计算源对象本地变化量矩阵
+        ###计算源对象本地矩阵
+        diver_local_matrix_node = pm.createNode(
+            "multMatrix", name=f"{driver}_local_matrix"
+        )
+        driver.worldMatrix[0].connect(diver_local_matrix_node.matrixIn[0])
+        driver.parentInverseMatrix[0].connect(diver_local_matrix_node.matrixIn[1])
+        ### 计算本地逆矩阵
+        driver_world_matrix = driver.worldMatrix[0].get()
+        driver_parentInverseMatrix = driver.parentInverseMatrix[0].get()
+        inverse_local_matrix = (
+            driver_world_matrix * driver_parentInverseMatrix
+        ).inverse()
+        ### 对象动态本地矩阵 * 本地静态逆矩阵 = 变化量矩阵
+        diver_local_matrix_node.matrixIn[2].set(inverse_local_matrix)
+
+        ## 获取Twist四元数
+        driver_quat_node = pm.createNode("decomposeMatrix", name=f"{driver}_quat")
+        diver_local_matrix_node.matrixSum.connect(driver_quat_node.inputMatrix)
+        driver_twist_node = pm.createNode("quatNormalize", name=f"{driver}_twist_quat")
+        driver_quat_node.outputQuatW.connect(driver_twist_node.inputQuatW)
+        driver_quat_node.attr(f"outputQuat{twist_axis}").connect(
+            driver_twist_node.attr(f"inputQuat{twist_axis}")
+        )
+        ## 获取Swing四元数
+        twist_invert_node = pm.createNode("quatInvert", name=f"{driver}_twist_invert")
+        driver_twist_node.outputQuat.connect(twist_invert_node.inputQuat)
+        driver_swing_node = pm.createNode("quatProd", name=f"{driver}_swing")
+        twist_invert_node.outputQuat.connect(driver_swing_node.input1Quat)
+        driver_quat_node.outputQuat.connect(driver_swing_node.input2Quat)
+        ## 设置Twist和Swing的四元数插值
+        twist_slerp_node = pm.createNode("quatSlerp", name=f"{driver}_twist_slerp")
+        twist_slerp_node.input1QuatW.set(1)
+        twist_slerp_node.inputT.set(twist)
+        driven.twist.connect(twist_slerp_node.inputT)
+        driver_twist_node.outputQuat.connect(twist_slerp_node.input2Quat)
+
+        swing_slerp_node = pm.createNode("quatSlerp", name=f"{driver}_swing_slerp")
+        swing_slerp_node.input1QuatW.set(1)
+        swing_slerp_node.inputT.set(swing)
+        driven.swing.connect(swing_slerp_node.inputT)
+        driver_swing_node.outputQuat.connect(swing_slerp_node.input2Quat)
+
+        slerp_quat_node = pm.createNode("quatProd", name=f"{driver}_slerp_quat")
+        twist_slerp_node.outputQuat.connect(slerp_quat_node.input1Quat)
+        swing_slerp_node.outputQuat.connect(slerp_quat_node.input2Quat)
+        ## 计算目标对象偏移矩阵
+        slerp_matrix_node = pm.createNode(
+            "composeMatrix", name=f"{driver}_slerp_matrix"
+        )
+        slerp_matrix_node.useEulerRotation.set(0)
+        slerp_quat_node.outputQuat.connect(slerp_matrix_node.inputQuat)
+
+        driven_offset_matrix_node = pm.createNode(
+            "multMatrix", name=f"{driven}_offset_matrix"
+        )
+        driven_worldMatrix = driven.worldMatrix[0].get()
+        driven_parentInverseMatrix = driven.parentInverseMatrix[0].get()
+        driven_local_matrix = driven_worldMatrix * driven_parentInverseMatrix
+
+        slerp_matrix_node.outputMatrix.connect(driven_offset_matrix_node.matrixIn[0])
+        driven_offset_matrix_node.matrixIn[1].set(driven_local_matrix)
+        ## 连接最终计算出的矩阵到目标父偏移矩阵
+        driven_offset_matrix_node.matrixSum.connect(driven.offsetParentMatrix)
+        ## 目标对象属性置零
+        for attr in [
+            f"{attr}{axis}" for attr in ["translate", "rotate"] for axis in "XYZ"
+        ]:
+            is_locked = driven.attr(attr).isLocked()
+            if is_locked:
+                driven.attr(attr).setLocked(False)
+            driven.attr(attr).set(0)
+            if is_locked:
+                driven.attr(attr).setLocked(True)
+        if driven_is_joint:
+            for attr in [f"jointOrient{axis}" for axis in "XYZ"]:
+                is_locked = driven.attr(attr).isLocked()
+                if is_locked:
+                    driven.attr(attr).setLocked(False)
+                driven.attr(attr).set(0)
+                if is_locked:
+                    driven.attr(attr).setLocked(True)
